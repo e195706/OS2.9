2aa592893ec4515abf30777c176369ba4e5e520a
e195706 <57646271+e195706@users.noreply.github.com>
2023-12-23 17:37:26 +0900

    Update GenaratePASS.py
    second commit

--- GenaratePASS.py
+++ GenaratePASS.py
@@ -5,7 +5,7 @@ def GenaratePASS():
     all_word = string.ascii_letters + string.digits + string.punctuation
     for i in range(10):
         password = []
-        for j in range(12):
+        for j in range(15):
             word=random.choice(all_word)
             password.append(word)
         print(''.join(password))
