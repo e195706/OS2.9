2c7f4234d3d4532009cc331e947e20a8c8bf5785
e195706 <57646271+e195706@users.noreply.github.com>
2024-01-01 16:26:53 +0900

    Update GenaratePASS.py
    記号の種類を内閣セキュリティーセンターの条件に合わせて26種類に変更しました。

--- GenaratePASS.py
+++ GenaratePASS.py
@@ -2,7 +2,11 @@ import random
 import string
 
 def GenaratePASS():
-    all_word = string.ascii_letters + string.digits + string.punctuation
+    all_word = string.ascii_letters + string.digits
+    symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?'
+    
+    all_word += symbols
+
     for i in range(10):
         password = []
         for j in range(15):
